9a8e992bc0b950fab4675ff06f6b3cfc3076941b
JaswanthRemiel <jaswanthremiel7@gmail.com>
2025-03-31 14:18:46 +0530

    changed

--- components/header.tsx
+++ components/header.tsx
@@ -9,6 +9,8 @@ import { Newsreader } from "next/font/google";
 import { LinksComponent } from "./linkscomponent";
 import { Typewriter } from "./ui/typewriter";
 import { InfiniteSliderBasic } from "./InfiniteSliderBasic";
+import { AnimatedShinyText } from "./magicui/animated-shiny-text";
+import { cn } from "@/lib/utils";
 
 const newsreader = Newsreader({ subsets: ["latin"], weight: ["300"] });
 
@@ -20,6 +22,21 @@ export function Header() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
+      <div className="z-6 flex items-center justify-left">
+        <div
+          className={cn(
+            "group rounded-sm border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
+          )}
+        >
+          <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+            <span>
+              <Link href={`${links.github}/jaswanthremiel.github.io`}>
+                *under construction
+              </Link>
+            </span>
+          </AnimatedShinyText>
+        </div>
+      </div>
       <div className="flex-col items-start space-y-5">
         <p className="whitespace-pre-wrap text-2xl font-sm">
           <p className="text-gray-100">{"hi i'm remiel"}</p>
