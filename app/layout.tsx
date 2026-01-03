42bec786a88997511c577013d4fef55c3b6b80db
Jaswanth Remiel AG <jaswanthremiel7@gmail.com>
2026-01-04 01:40:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,26 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
+  title: 'Github Remiel',
+  description: 'Github Remiel',
+  generator: 'Github Remiel',
 }
 
 export default function RootLayout({
